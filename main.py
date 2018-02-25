410712dc548482f76f70b952a71fbc07a4138aa3
Benjamin Wu <benwufc@gmail.com>
2018-02-25 22:18:42 +0800

    creating new folder based on picture's time
    this version base on picture exif time to creat  the folder
    and copy picture to there and sort the picture base on taking picture time.

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import os
 import exifread
-path = "/home/ben/Pictures/2018-02-16_Thailand_travel_HuaHin_LetsSeaResort"
+import time
+import shutil
+#===== adjusted parameter =====
+path = "/home/benjamin/Pictures/"
+folder = "test_folder"
+file_header_name = "Thai_travel"
+#==============================
+PWD = os.path.join(path, folder)
+EXTENSIONS = (".jpg", "jpeg", ".JPG", ".JPEG")
+
+def get_exif_date(fn):
+    f = open(os.path.join(PWD, fn),'rb')
+    if os.path.splitext(fn)[-1] in EXTENSIONS:
+        tags_time = "%s" % exifread.process_file(f)["EXIF DateTimeOriginal"]
+        return tags_time.replace(":", "-")
+    else:
+        print "this file:", fn, " isn't jpg file."
+    f.close()
+
+
 pic_list = []
-pic_timestamp = []
-video_list = []
-for file_name in os.listdir(path):
-    if "JPG" in file_name:
-        pic_list.append(file_name)
-        f = open(path + '/' + file_name, 'rb')
-        time_tags = '%s' % exifread.process_file(f)["EXIF DateTimeOriginal"]
-        pic_timestamp.append(time_tags)
-    elif "mov" in file_name:
-        video_list.append(file_name)
+
+for fn in os.listdir(PWD):
+    pic_time = get_exif_date(fn)
+    pic_list.append((pic_time, fn))
+for idx, (pic_time, fn) in enumerate(sorted(pic_list)):
+    pic_day = pic_time[0:10]
+    f_name_day = file_header_name + '_' + pic_day
+    new_folder = os.path.join(path, f_name_day)
+    original_file = os.path.join(PWD, fn)
+    new_file_name = f_name_day + "_" + str(idx) 
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
+    shutil.copy(original_file, new_folder)
+    os.rename(os.path.join(new_folder, fn), os.path.join(new_folder, new_file_name) )
 
 
-for idx, (time, file_name) in enumerate(sorted(zip(pic_timestamp, pic_list))):
-    os.rename(path + '/' + file_name, path + '/LetsSeaResort_'+ str(idx))
-    
